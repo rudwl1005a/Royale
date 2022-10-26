20cc4237589c7f1299db387619cb6dda59a05964
민경대 <rudwl1005@naver.com>
2022-10-26 18:10:02 +0900

    [INITIAL] 스코어보드 기본 구조

---
+++ frontend/src/components/Scoreboard/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { FaFacebookF, FaTwitch, FaTwitter, FaYoutube } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+import "./style.css";
+
+function Register(props) {
+  const onClick = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <div className="page-404 section--full-bg">
+        <Container>
+          HI
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default Register;
