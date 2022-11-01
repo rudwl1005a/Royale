f3c356c37de063b6540e5997cb74a8e9d1e7d018
안세영 <tpdud285@naver.com>
2022-11-01 09:49:36 +0900

    [ADD] Tests수정

--- backend/royale/src/test/java/com/ssafy/royale/RoyaleApplicationTests.java
+++ backend/royale/src/test/java/com/ssafy/royale/RoyaleApplicationTests.java
@@ -1,17 +1,8 @@
 package com.ssafy.royale;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RoyaleApplicationTests {
-
-	@Test
-	void 대진표매칭테스트() {
-		int[] arr = new int[] {1,2,3,1,1,2,4,4};
-		int[] result = new int[8];
-
-
-	}
 
 }
