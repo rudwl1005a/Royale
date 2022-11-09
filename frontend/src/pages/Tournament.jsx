db87c9e5db31048b8e7ed1f5de543cb1eddde959
안세영 <tpdud285@naver.com>
2022-11-09 09:16:53 +0900

    [UPDATE] 토너먼트 테마 추가

--- frontend/src/pages/Tournament.jsx
+++ frontend/src/pages/Tournament.jsx
@@ -2,7 +2,7 @@ import { React, useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import {
   SingleEliminationBracket,
-  Match,
+  Match, create
 } from "@g-loot/react-tournament-brackets";
 import { axios } from "../api/axios.js";
 
@@ -59,12 +59,29 @@ export default function Daejin(props) {
     }
   }
 
+const WhiteTheme = createTheme({
+  textColor: { main: '#000000', highlighted: '#07090D', dark: '#3E414D' },
+  matchBackground: { wonColor: '#daebf9', lostColor: '#96c6da' },
+  score: {
+    background: { wonColor: '#87b2c4', lostColor: '#87b2c4' },
+    text: { highlightedWonColor: '#7BF59D', highlightedLostColor: '#FB7E94' },
+  },
+  border: {
+    color: '#CED1F2',
+    highlightedColor: '#da96c6',
+  },
+  roundHeader: { backgroundColor: '#da96c6', fontColor: '#000' },
+  connectorColor: '#CED1F2',
+  connectorColorHighlight: '#da96c6',
+  svgBackground: '#FAFAFA',
+});
+  
   return (
     <div>
       {/* <div>{params.leagueSeq}경기 {params.divisionSeq}부문 대진표</div> */}
       {matches ? (
         <div onClick={A}>
-          <SingleEliminationBracket matches={matches} matchComponent={Match} />
+          <SingleEliminationBracket matches={matches} matchComponent={Match} theme={ WhiteTheme} />
         </div>
       ) : (
         ""
