1c8865f18158c1d8f74052fc14499588718d55a8
조찬홍 <choch953@gmail.com>
2022-11-19 12:46:12 +0900

    [UPDATE] Footer 원상복귀

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -25,159 +25,109 @@ function Footer(props) {
         <div className="footer-top-area">
           <Container>
             <Row>
-              <Col lg={1} />
               <Col lg={4} sm={6}>
                 <div className="single-footer">
                   <h3>About us</h3>
                   <p>
-                  리그로얄은 대한민국 최고의 주짓수 대회입니다.
+                    Etiam consequat sem ullamcorper, euismod metus sit amet,
+                    tristique justo. Vestibulum mattis, nisi ut faucibus
+                    commodo, risus ex commodo.
                   </p>
                   <p>
-                  LeagueRoyale is the most awaited jiu jitsu events in South Korea
+                    euismod metus sit amet, tristique justo. Vestibulum mattis
+                    Vestibulum mattis,{" "}
                   </p>
                 </div>
               </Col>
-              <Col lg={2} />
-              <Col lg={4} sm={6}>
-                <div className="single-footer">
-                  <h3>Contact Us</h3>
-                  <div className="footer-contact">
-                    <h4 className="title">
-                      <FaMapMarkerAlt />
-                      Address
-                    </h4>
-                    <p>
-                      부산광역시 부산진구 서면로
-                      <br />
-                      부전동 51 지하1층
-                    </p>
-                  </div>
-                  <div className="footer-contact">
-                    <h4 className="title">
-                      <FaEnvelope />
-                      Email Address
-                    </h4>
-                    <p> royale@royale.kr</p>
-                  </div>
-                  <div className="footer-contact">
-                    <h4 className="title">
-                      <FaPhoneAlt />
-                      Phone
-                    </h4>
-                    <p>010-9885-9195</p>
-                  </div>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-        <div className="footer-bottom">
-          <Container>
-            <Row>
-              <Col sm={12}>
-                <div className="footer-bottom-inn">
-                  <div className="footer-logo">
-                    <a href="/">
-                      <img src={logo} alt="site logo" />
-                    </a>
-                  </div>
-                  <div className="footer-social">
+              <Col lg={2} sm={6}>
+                <div className="widget-content">
+                  <div className="single-footer">
+                    <h3>Explore</h3>
                     <ul>
                       <li>
-                        <a
-                          href="https://ko-kr.facebook.com/leagueroyale/"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <FaFacebookF />
-                        </a>
+                        <Link to="/" onClick={onClick}>
+                          <FaCaretRight />
+                          About
+                        </Link>
                       </li>
                       <li>
-                        <a
-                          href="https://www.instagram.com/jiujitsuroyale/"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <FaInstagram />
-                        </a>
+                        <Link to="/" onClick={onClick}>
+                          <FaCaretRight />
+                          Our Games
+                        </Link>
                       </li>
                       <li>
-                        <a
-                          href="https://www.youtube.com/channel/UCeSQZrz7nc-ABi9W7xqSpnA"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <FaYoutube />
-                        </a>
+                        <Link to="/" onClick={onClick}>
+                          <FaCaretRight />
+                          Contact Us
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/" onClick={onClick}>
+                          <FaCaretRight />
+                          Help &amp; Support
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/" onClick={onClick}>
+                          <FaCaretRight />
+                          Privacy Policy
+                        </Link>
                       </li>
                     </ul>
                   </div>
-                  <div className="copyright">
-                    <p>&copy; Copyrights Royale - All Rights Reserved</p>
+                </div>
+              </Col>
+              <Col lg={3} sm={6}>
+                <div className="widget-content">
+                  <div className="single-footer">
+                    <h3>Our Games</h3>
+                    <ul>
+                      <li>
+                        <Link to="/" onClick={onClick}>
+                          <FaCaretRight />
+                          Need For Speed
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/" onClick={onClick}>
+                          <FaCaretRight />
+                          Call Of Duty
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/" onClick={onClick}>
+                          <FaCaretRight />
+                          Resident Evil
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/" onClick={onClick}>
+                          <FaCaretRight />
+                          Dragons Fight
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/" onClick={onClick}>
+                          <FaCaretRight />2 Player Champions
+                        </Link>
+                      </li>
+                    </ul>
                   </div>
                 </div>
               </Col>
-            </Row>
-          </Container>
-        </div>
-      </footer>
-    </>
-  );
-}
-
-export default Footer;
-import React from "react";
-import { Link } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
-import {
-  FaCaretRight,
-  FaMapMarkerAlt,
-  FaEnvelope,
-  FaPhoneAlt,
-  FaFacebookF,
-  FaInstagram,
-  FaYoutube,
-} from "react-icons/fa";
-import logo from "../../img/Logo_LeagueRoyale_black.png";
-
-import "./style.css";
-
-function Footer(props) {
-  const onClick = (e) => {
-    e.preventDefault();
-  };
-
-  return (
-    <>
-      <footer className="fag-footer">
-        <div className="footer-top-area">
-          <Container>
-            <Row>
-              <Col lg={1} />
-              <Col lg={4} sm={6}>
-                <div className="single-footer">
-                  <h3>About us</h3>
-                  <p>
-                  리그로얄은 대한민국 최고의 주짓수 대회입니다.
-                  </p>
-                  <p>
-                  LeagueRoyale is the most awaited jiu jitsu events in South Korea
-                  </p>
-                </div>
-              </Col>
-              <Col lg={2} />
-              <Col lg={4} sm={6}>
+              <Col lg={3} sm={6}>
                 <div className="single-footer">
                   <h3>Contact Us</h3>
                   <div className="footer-contact">
                     <h4 className="title">
                       <FaMapMarkerAlt />
-                      Address
+                      Address{" "}
                     </h4>
                     <p>
-                      부산광역시 부산진구 서면로
+                      88 road, broklyn street
                       <br />
-                      부전동 51 지하1층
+                      new york, usa
                     </p>
                   </div>
                   <div className="footer-contact">
@@ -185,14 +135,14 @@ function Footer(props) {
                       <FaEnvelope />
                       Email Address
                     </h4>
-                    <p> royale@royale.kr</p>
+                    <p>info@example.com</p>
                   </div>
                   <div className="footer-contact">
                     <h4 className="title">
                       <FaPhoneAlt />
-                      Phone
+                      Phone{" "}
                     </h4>
-                    <p>010-9885-9195</p>
+                    <p>777-1234-567</p>
                   </div>
                 </div>
               </Col>
